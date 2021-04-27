d481edd1682877d5581fc8332216979bf1d63780
Fernando Mendonça de Almeida <fernando.m.al.91@gmail.com>
2021-04-26 22:06:17 -0300

    First version of Commnad Line

--- cmd/frunner/main.go
+++ cmd/frunner/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/fernandomalmeida/frunner"
+	"gopkg.in/yaml.v3"
+)
 
 func main() {
-	fmt.Println("Not implemented yet")
+	if len(os.Args) < 2 {
+		fmt.Printf("Use with DIR argument: %s DIR\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	dir := os.Args[1]
+
+	pipelineBytes, err := ioutil.ReadFile(filepath.Join(dir, ".frunner.yaml"))
+	if err != nil {
+		log.Fatalf("error on read yaml file: %s", err)
+	}
+
+	var pipeline frunner.Pipeline
+	err = yaml.Unmarshal(pipelineBytes, &pipeline)
+	if err != nil {
+		log.Fatalf("error on read yaml file: %s", err)
+	}
+
+	pipeline.FillDir(dir)
+
+	for i := 0; i < len(pipeline.Steps); i++ {
+		step := pipeline.Steps[i]
+		err := step.Run()
+		if err != nil {
+			log.Fatalf("error on step %s: %s", step.Name, err)
+		}
+	}
+
 }
